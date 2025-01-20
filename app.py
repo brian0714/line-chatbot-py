88c85bc33e64f91d230d82738d7f1e439bc5af89
Brian <brian2000890714@gmail.com>
2025-01-21 02:17:45 +0800

    Modify requirements.txt for package depreciation

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import os
+from dotenv import load_dotenv
+
+# 載入 .env 至地端中
+# load_dotenv()
 
 app = Flask(__name__)
 
@@ -22,11 +26,11 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
+# where the bot handle user input
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
